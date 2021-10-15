1a8c23728a657e8d757a6c4c5675a620744bae9c
siikamiika <siikamiika@users.noreply.github.com>
2021-10-15 18:41:32 +0300

    generate json

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 # Todo
 
 - [x] Focus tabs using fzf launcher
-- [ ] Generate native messaging host descriptor json dynamically
+- [x] Generate native messaging host descriptor json dynamically
 - [ ] More reliable focus condition by adding URL (or windowId) to window title, see https://github.com/erichgoldman/add-url-to-window-title
 - [ ] Use Unix domain sockets
 - [ ] Don't depend on `swaymsg`
--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
-7z a tab_control@miika.es.xpi -tzip \
+export APP_ID="$(jq -r '.applications.gecko.id' ./manifest.json)"
+export NAME="tab_control"
+export DESC="Host for tab control"
+export BIN_PATH="/usr/local/bin/tab_control.py"
+export TYPE="stdio"
+
+# build extension
+7z a \
+    "$APP_ID.xpi" \
+    -tzip \
     ./manifest.json \
     ./bg
+
+# build native messaging host
+jq -Rn '{
+    name: env.NAME,
+    description: env.DESC,
+    path: env.BIN_PATH,
+    type: env.TYPE,
+    allowed_extensions: [env.APP_ID]
+}' > "$NAME.json"
