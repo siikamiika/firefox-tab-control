ba3548d4f20ecdf6a55d8e17716ee494938ff692
siikamiika <siikamiika@users.noreply.github.com>
2021-10-17 01:53:36 +0300

    use proper shell for DMENU

--- tab_control.py
+++ tab_control.py
@@ -75,7 +75,7 @@ class FirefoxTabController(object):
             url = tab['url']
             input_lines.append(f'{tab_id} {sound}{title} ({url})')
 
-        p = subprocess.Popen(os.getenv('DMENU').split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        p = subprocess.Popen(os.getenv('DMENU'), stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
         p.stdin.write(('\n'.join(input_lines) + '\n').encode('utf-8'))
         p.stdin.close()
         selected_tab = p.stdout.read()
